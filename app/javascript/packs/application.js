d300d3dcd86c7ebeadc07c12c7d26d7a3385d4a6
jrtiff0221 <jrtiff0221@gmail.com>
2020-06-12 23:08:31 -0600

    Add bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,30 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
+
+import $ from 'jquery';
+import 'bootstrap/dist/js/bootstrap';
+import "@fortawesome/fontawesome-free/js/all";
+
+$(document).on('turbolinks:load', function() {
+  $('body').tooltip({
+    selector: '[data-toggle="tooltip"]',
+    container: 'body',
+  });
+
+  $('body').popover({
+    selector: '[data-toggle="popover"]',
+    container: 'body',
+    html: true,
+    trigger: 'hover',
+  });
+});
