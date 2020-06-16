89f72450ee1b0baf4c437b0ecff4b0f1804d3bae
jrtiff0221 <jrtiff0221@gmail.com>
2020-06-15 22:43:40 -0600

    wip

--- app/models/produce.rb
+++ app/models/produce.rb
@@ -4,13 +4,10 @@ class Produce < ApplicationRecord
 
     has_many :gardens
     has_many :users, through: :gardens
+    accepts_nested_attributes_for :gardens
     
     scope :order_by_category, -> {order(:category)}
-    scope :vegetables, -> {where(category: "Vegetable")}
-    scope :fruits, -> {where(category: "Fruit")}
-    scope :herbs, -> {where(category: "Herb")}
-    scope :flowers, -> {where(category: "Flower")}
-    scope :miscellaneous, ->{where(category: "Miscellanous")}
+
 
     scope :order_by_category, -> {order(:category)}
     scope :search_by_category, -> (search_category){where("category = ?", search_category)}
